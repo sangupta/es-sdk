0e09d38aabb87c7027246ab37b29deb4bcbaebb5
Sandeep Gupta <sandy.pec@gmail.com>
2015-09-04 14:58:22 +0530

    Added getter/setters

--- src/main/java/com/sangupta/esclient/domain/IndexedDocument.java
+++ src/main/java/com/sangupta/esclient/domain/IndexedDocument.java
@@ -25,4 +25,104 @@ public class IndexedDocument {
 	@SerializedName("_source")
 	private Object source;
 	
+	// Usual accessors follow
+
+	/**
+	 * @return the index
+	 */
+	public String getIndex() {
+		return index;
+	}
+
+	/**
+	 * @param index the index to set
+	 */
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the version
+	 */
+	public long getVersion() {
+		return version;
+	}
+
+	/**
+	 * @param version the version to set
+	 */
+	public void setVersion(long version) {
+		this.version = version;
+	}
+
+	/**
+	 * @return the found
+	 */
+	public boolean isFound() {
+		return found;
+	}
+
+	/**
+	 * @param found the found to set
+	 */
+	public void setFound(boolean found) {
+		this.found = found;
+	}
+
+	/**
+	 * @return the score
+	 */
+	public double getScore() {
+		return score;
+	}
+
+	/**
+	 * @param score the score to set
+	 */
+	public void setScore(double score) {
+		this.score = score;
+	}
+
+	/**
+	 * @return the source
+	 */
+	public Object getSource() {
+		return source;
+	}
+
+	/**
+	 * @param source the source to set
+	 */
+	public void setSource(Object source) {
+		this.source = source;
+	}
+	
 }
